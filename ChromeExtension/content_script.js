93ef75db1c4b85066eeaa5efcdabcb11d508daab
Alen Pelin <AlenPelin@users.noreply.github.com>
2018-03-27 21:43:23 +1100

    Add chrome extension

---
+++ ChromeExtension/content_script.js
@@ -0,0 +1,51 @@
+var mercury = new XMLHttpRequest();
+mercury.open('GET', 'https://mercury.postlight.com/parser?url=' + window.document.location.href, true);
+mercury.setRequestHeader('Content-Type', 'application/json');
+mercury.setRequestHeader('x-api-key', '6D2GolgQymAhEgpHsXrKEvwasxWSRSPWB5K1An3x');
+
+mercury.onreadystatechange = function() {
+  if(mercury.readyState == 4 && mercury.status == 200) {
+    const obj = JSON.parse(mercury.responseText);
+    console.log('mecury success', obj);
+    mercurySuccess(obj);
+  }
+}
+
+mercury.send();
+
+function mercurySuccess(data){
+  var blueCopy = new XMLHttpRequest();
+  blueCopy.open('POST', 'http://localhost:5000/api/v1/content?redirect=false', true);
+  blueCopy.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+  
+  blueCopy.onreadystatechange = function() {
+    if(blueCopy.readyState == 4 && blueCopy.status == 200) {
+      const obj = JSON.parse(blueCopy.responseText);
+      console.log('blue copy success', obj);
+      blueCopySuccess(obj);
+    }
+  }
+
+  var postData = getBlueCopyPostData(data);
+  
+  blueCopy.send(postData);
+}
+
+function blueCopySuccess(url){
+  document.location.href = url;
+}
+
+function getBlueCopyPostData(data) { 
+  return 'content=' 
+    + encodeURIComponent(`
+<html>
+  <head>
+    <title>%COPYBLUETITLE%</title>
+    <link rel="amphtml" href="https://mercury.postlight.com/amp?url=%COPYBLUEURL%">
+  </head>
+  <body>
+    ${data.content}
+  </body>
+</html>
+    `.trim());
+}
